6a96df1882d701d67da8a5c65c712d7f7c5e01a0
钟海 <417233956@qq.com>
2019-07-08 18:43:38 +0800

    发布更新包，更新使用demo

--- WebDemo/WebComponentDemo/ClientAPP/src/app/app.component.ts
+++ WebDemo/WebComponentDemo/ClientAPP/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'webapp';
+  title = '用户选择';
+  bodyStyle = { 'width.px': 100, 'height.px': 100 };
+  constructor() { }
+  mgOnOk(data): void {
+    // tslint:disable-next-line:no-console
+    console.info('所选择的数据:' + JSON.stringify(data));
+  }
+  mgOnCancel(data): void {
+    // tslint:disable-next-line:no-console
+    console.info(data);
+  }
+  mgAfterClose(data): void {
+    // tslint:disable-next-line:no-console
+    console.info(data);
+  }
+  mgAfterOpen(data): void {
+    // tslint:disable-next-line:no-console
+    console.info(data);
+  }
 }
